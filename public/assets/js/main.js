72e175da1503b3a887bbf7270294e46f37f6be37
goldsteinsveta <svetlana.goldstein@gmail.com>
2019-03-04 14:18:38 +0100

    Terms and conditions for every login

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,26 +2,59 @@ var terms = {}
 
 $(document).ready(function(){
 
-	terms.popup = $('.popup-box')
-	terms.input = $('#inputTerms')
-	terms.btn = $('.popup-box .btn-action')
-	
-	if (Cookies.get('AXS_terms') != '1') {
-		terms.popup.fadeIn()
-	}
+	function updateCoo() {
 
-	terms.input.on('change', function(){
-		if ($(this).is(':checked')) {
-			terms.btn.prop('disabled', false)
+		terms.popup = $('.popup-box')
+		terms.input = $('#inputTerms')
+		terms.btn = $('.popup-box .btn-action')
+		
+		if (Cookies.get('AXS_terms') != '1') {
+			terms.popup.fadeIn()
+		}
+
+		terms.input.on('change', function(){
+			if ($(this).is(':checked')) {
+				terms.btn.prop('disabled', false)
+				return
+			}
+			terms.btn.prop('disabled', true)
+		})
+
+		terms.btn.on('click', function(){
+			if (terms.btn.prop('disabled')) return
+			Cookies.set('AXS_terms', '1')
+			terms.popup.fadeOut()
+		})
+
+	}
+	updateCoo()
+
+	function waitForUpdate(){
+		terms.update = $('.display-4')
+		console.log(0)
+		if (terms.update.length == 0) {
+			setTimeout(function(){
+				waitForUpdate()	
+			}, 500)
 			return
 		}
-		terms.btn.prop('disabled', true)
-	})
+			Cookies.remove('AXS_terms')
+		waitForLogin()
+	}
+	waitForUpdate()
 
-	terms.btn.on('click', function(){
-		if (terms.btn.prop('disabled')) return
-		Cookies.set('AXS_terms', '1')
-		terms.popup.fadeOut()
-	})
+	function waitForLogin() {
+		terms.update = $('.display-4')
+		console.log(1)
+		if (terms.update.length == 1) {
+			setTimeout(function(){
+				waitForLogin()	
+			}, 500)
+			return
+		}
+		updateCoo()
+		waitForUpdate()
+	}
 	
 })
+
